58907eeb75ccfcdc9323e5a48c6d5a95bacc484b
pengleo5422 <leo542221@gmail.com>
2023-11-21 21:46:30 +0800

    語言

---
+++ lib/generated/intl/messages_en.dart
@@ -0,0 +1,120 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  static String m0(a) => "You have been like this \nfor ${a} minutes today";
+
+  static String m1(date) => "Every ${date} bad posture";
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "Cross_Legged": MessageLookupByLibrary.simpleMessage("Cross-Legged"),
+        "Hunchback": MessageLookupByLibrary.simpleMessage("Hunched"),
+        "Left_foot": MessageLookupByLibrary.simpleMessage("Left Leg Crossed"),
+        "OK": MessageLookupByLibrary.simpleMessage("OK"),
+        "One_third": MessageLookupByLibrary.simpleMessage("One-Third Sitting"),
+        "Sit": MessageLookupByLibrary.simpleMessage("Normal Sitting"),
+        "Sphinx": MessageLookupByLibrary.simpleMessage("Sphinx"),
+        "Today_min": m0,
+        "Upper_left":
+            MessageLookupByLibrary.simpleMessage("Upper Body Left Tilt"),
+        "Upper_right":
+            MessageLookupByLibrary.simpleMessage("Upper Body Right Tilt"),
+        "WIFI":
+            MessageLookupByLibrary.simpleMessage("Seat cushion Wi-Fi settings"),
+        "ad": MessageLookupByLibrary.simpleMessage(
+            "Your recent bad posture is mostly leaning to the right. We recommend that you refer to the following rehabilitation exercises"),
+        "already_connect":
+            MessageLookupByLibrary.simpleMessage("Seat cushion is connected"),
+        "av_sit":
+            MessageLookupByLibrary.simpleMessage("Daily bad sitting time"),
+        "con": MessageLookupByLibrary.simpleMessage("Continue"),
+        "connect": MessageLookupByLibrary.simpleMessage("Connecting"),
+        "connect_sit": MessageLookupByLibrary.simpleMessage("Connect"),
+        "day": MessageLookupByLibrary.simpleMessage("Day"),
+        "detail": MessageLookupByLibrary.simpleMessage("Detailed data"),
+        "detail_record":
+            MessageLookupByLibrary.simpleMessage("Detailed record"),
+        "en_account_name":
+            MessageLookupByLibrary.simpleMessage("Enter account name"),
+        "en_pass": MessageLookupByLibrary.simpleMessage("Enter password"),
+        "enter_your": MessageLookupByLibrary.simpleMessage("Enter your"),
+        "ever_ad":
+            MessageLookupByLibrary.simpleMessage("Daily recommendations"),
+        "every_awful": m1,
+        "female": MessageLookupByLibrary.simpleMessage("Female"),
+        "fun": MessageLookupByLibrary.simpleMessage("Functions"),
+        "gender": MessageLookupByLibrary.simpleMessage("Gender"),
+        "half_lying":
+            MessageLookupByLibrary.simpleMessage("Half-Lying Half-Sitting"),
+        "height": MessageLookupByLibrary.simpleMessage("Height"),
+        "hi": MessageLookupByLibrary.simpleMessage("Hi, "),
+        "hn_account": MessageLookupByLibrary.simpleMessage(
+            "Don\'t have an account yet\t？"),
+        "hour": MessageLookupByLibrary.simpleMessage("Hour"),
+        "in_a": MessageLookupByLibrary.simpleMessage(
+            "Please enter your account name\nto continue to the next step"),
+        "login": MessageLookupByLibrary.simpleMessage("Login"),
+        "logout": MessageLookupByLibrary.simpleMessage("Logout"),
+        "low": MessageLookupByLibrary.simpleMessage("Low latency"),
+        "low_than": MessageLookupByLibrary.simpleMessage("Less than "),
+        "male": MessageLookupByLibrary.simpleMessage("Male"),
+        "min": MessageLookupByLibrary.simpleMessage("min"),
+        "min_av": MessageLookupByLibrary.simpleMessage("Average min"),
+        "month": MessageLookupByLibrary.simpleMessage("Month"),
+        "name": MessageLookupByLibrary.simpleMessage("Account name"),
+        "next_step": MessageLookupByLibrary.simpleMessage("Next step"),
+        "now": MessageLookupByLibrary.simpleMessage("Currently, you\n"),
+        "password": MessageLookupByLibrary.simpleMessage("Password"),
+        "re_d": MessageLookupByLibrary.simpleMessage("Modify information"),
+        "realtime": MessageLookupByLibrary.simpleMessage("Real-time"),
+        "reg": MessageLookupByLibrary.simpleMessage("Register"),
+        "right_foot": MessageLookupByLibrary.simpleMessage("Right Leg Crossed"),
+        "see_static":
+            MessageLookupByLibrary.simpleMessage("View statistical data"),
+        "set_pass": MessageLookupByLibrary.simpleMessage("Set your password"),
+        "setting": MessageLookupByLibrary.simpleMessage("Settings"),
+        "sit_ID":
+            MessageLookupByLibrary.simpleMessage("Seat cushion serial number"),
+        "sit_ID_code":
+            MessageLookupByLibrary.simpleMessage("Seat number: dwxsfexdzy"),
+        "st_pass": MessageLookupByLibrary.simpleMessage(
+            "Enter a strong password that is\nsufficient to protect your account"),
+        "step": MessageLookupByLibrary.simpleMessage("Step "),
+        "total_av": MessageLookupByLibrary.simpleMessage("Total average"),
+        "total_static":
+            MessageLookupByLibrary.simpleMessage("Total statistics"),
+        "total_time": MessageLookupByLibrary.simpleMessage("Data"),
+        "week": MessageLookupByLibrary.simpleMessage("Week"),
+        "weight": MessageLookupByLibrary.simpleMessage("Weight"),
+        "welcome": MessageLookupByLibrary.simpleMessage("Welcome back"),
+        "wifi_name": MessageLookupByLibrary.simpleMessage("Hotspot Wi-Fi name"),
+        "wifi_password":
+            MessageLookupByLibrary.simpleMessage("Hotspot Wi-Fi password"),
+        "year": MessageLookupByLibrary.simpleMessage("Year"),
+        "you_gender":
+            MessageLookupByLibrary.simpleMessage("What is your gender?"),
+        "your_height":
+            MessageLookupByLibrary.simpleMessage("What is your height?"),
+        "your_weight":
+            MessageLookupByLibrary.simpleMessage("What is your weight?")
+      };
+}
